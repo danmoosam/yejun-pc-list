78300b832995ae6178bdbfe107dbb3ba19712c16
이채문 <moon@ichaemun-ui-iMac.local>
2025-07-06 21:10:09 +0900

    fix: specify entry point in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // 루트는 현재 폴더
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html'),
+    },
+  },
 })
