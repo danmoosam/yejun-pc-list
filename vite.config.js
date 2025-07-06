c2cd59f9b296cceb7817fea3070652b6c089d993
이채문 <moon@ichaemun-ui-iMac.local>
2025-07-06 21:39:41 +0900

    fix: move index.html to root

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // 꼭 이렇게 루트 기준으로 설정
+    },
+  },
 })
