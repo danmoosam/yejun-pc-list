4ea773e282e8b04dd2226c948bc17603d8dbb2a5
이채문 <moon@ichaemun-ui-iMac.local>
2025-07-06 21:20:35 +0900

    update vite.config.js for build output

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // 루트는 현재 폴더
-  publicDir: 'public',
+  base: './',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
-    },
-  },
+    outDir: 'dist'
+  }
 })
